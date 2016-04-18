17096b1b8754bab70b950ed609c9973b82a3d18b
eugene <yannesh@gmail.com>
2016-04-18 21:30:26 +0300

    init client

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 
 app.listen(3000, function () {
   console.log('app started on port: ', 3000);
